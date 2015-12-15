5eabec34cd445f616b4f66944541c11c7b37b71d
manoramahp <manorama@wso2.com>
2015-12-15 17:56:48 +0530

    Added beacon log producer

---
+++ app/src/main/java/org/wso2/beaconlogproducer/BeaconReceiverActivity.java
@@ -0,0 +1,132 @@
+package org.wso2.beaconlogproducer;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+
+import java.util.Collection;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class BeaconReceiverActivity extends Activity implements BeaconConsumer {
+    protected static final String TAG = "MonitoringActivity";
+    private BeaconManager beaconManager;
+    private Queue<BeaconDataRecord> queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beacon_receiver);
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        // To detect proprietary beacons, you must add a line like below corresponding to your beacon
+        // type.  Do a web search for "setBeaconLayout" to get the proper expression.
+//         beaconManager.getBeaconParsers().add(new BeaconParser().
+//                setBeaconLayout("m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.bind(this);
+
+        queue = new ConcurrentLinkedQueue<BeaconDataRecord>();
+        new Thread() {
+            @Override
+            public void run() {
+
+                publishBeaconData();
+            }
+        }.start();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        beaconManager.unbind(this);
+    }
+    @Override
+    public void onBeaconServiceConnect() {
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(org.altbeacon.beacon.Region region) {
+                Log.i(TAG, "Entered beacon region");
+            }
+
+            @Override
+            public void didExitRegion(org.altbeacon.beacon.Region region) {
+                Log.i(TAG, "Exited beacon region");
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, org.altbeacon.beacon.Region region) {
+                Log.i(TAG, "Switched from seeing/not seeing beacons: "+i);
+            }
+        });
+
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> beacons, Region region) {
+                if (beacons.size() > 0) {
+                    Log.i(TAG, "Reading from beacon");
+
+                    while (beacons.iterator().hasNext()) {
+                        Beacon beacon = beacons.iterator().next();
+                        BeaconDataRecord beaconDataRecord = new BeaconDataRecord();
+                        beaconDataRecord.setUuid(String.valueOf(beacon.getId1()));
+                        beaconDataRecord.setMajor(String.valueOf(beacon.getId2()));
+                        beaconDataRecord.setMinor(String.valueOf(beacon.getId3()));
+                        queue.add(beaconDataRecord);
+                    }
+                }
+            }
+        });
+
+        try {
+            beaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+        } catch (RemoteException e) {    }
+    }
+
+
+    private void publishBeaconData() {
+
+        HttpClient httpClient = new DefaultHttpClient();
+
+        try {
+            // TODO add the ESB server url here
+            HttpPost httpPost = new HttpPost("http://10.100.7.15:8282/test-beacon");
+
+            while (true) {
+                JSONObject jsonObj = new JSONObject();
+                jsonObj.put("beaconUuid", "d3fbdbe3-e95b-41a4-8b73-8a02feb257ba");
+                jsonObj.put("beaconMajor", "1");
+                jsonObj.put("beaconMinor", "2");
+
+                BeaconDataRecord record = queue.poll();
+                if(record != null) {
+                    jsonObj.put("beaconUuid", record.getUuid());
+                    jsonObj.put("beaconMajor", record.getMajor());
+                    jsonObj.put("beaconMinor", record.getMinor());
+
+                }
+
+                StringEntity entity = new StringEntity(jsonObj.toString(), HTTP.UTF_8);
+                entity.setContentType("application/json");
+                httpPost.setEntity(entity);
+                HttpResponse response = httpClient.execute(httpPost);
+                response.getEntity().consumeContent();
+            }
+
+        } catch (Exception e) {
+            Log.e("Error : REST service", e.getMessage());
+        }
+    }
+}
