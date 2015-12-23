75314e16bfd2ee28aad98295fa7c9325f0a272fb
manoramahp <manorama@wso2.com>
2015-12-23 14:13:46 +0530

    Cleaning code

--- app/src/main/java/org/wso2/beaconlogproducer/BeaconReceiverActivity.java
+++ app/src/main/java/org/wso2/beaconlogproducer/BeaconReceiverActivity.java
@@ -34,7 +34,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Collection;
 import java.util.Date;
 import java.util.Iterator;
